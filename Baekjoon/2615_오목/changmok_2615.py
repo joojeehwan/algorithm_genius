bfa54e62eaef6e377744f5443c3580f21bb97a19
all-eviate <riverboy94@gmail.com>
2022-03-22 01:31:01 +0900

    주석 추가

--- Baekjoon/2615_오목/changmok_2615.py
+++ Baekjoon/2615_오목/changmok_2615.py
@@ -3,6 +3,15 @@ input = sys.stdin.readline
 
 board = [list(map(int, input().split())) for _ in range(19)]
 
+# 각 체크 함수의 로직
+# 다음 조건을 만족하는 한 정해진 방향으로 계속 가며 돌을 체크한다.
+#     바둑판을 벗어나지 않는다.
+#     다른 돌을 마주친다.
+#     5개가 넘는 연속된 돌을 본다.
+# 이동을 멈췄을 때 만난 돌이 정확히 다섯개인 경우 다음 조건을 판단
+#     시작 돌이 테두리이거나 시작 돌 이전으로 연속이 이어지지 않는 경우 참 반환
+# 그 외의 경우 거짓 반환
+
 def horizontalCheck(r, c, n):
     start = r
     chain = 0
@@ -12,8 +21,7 @@ def horizontalCheck(r, c, n):
     if chain == 5:
         if start == 0 or board[start-1][c] != n:
             return True
-        else:
-            return False
+    return False
 
 def diagonalCheckDR(r, c, n):
     sr = r
@@ -26,8 +34,7 @@ def diagonalCheckDR(r, c, n):
     if chain == 5:
         if sr == 0 or sc == 0 or board[sr-1][sc-1] != n:
             return True
-        else:
-            return False
+    return False
 
 def verticalCheck(r, c, n):
     start = c
@@ -38,8 +45,7 @@ def verticalCheck(r, c, n):
     if chain == 5:
         if start == 0 or board[r][start-1] != n:
             return True
-        else:
-            return False
+    return False
 
 def diagonalCheckUR(r, c, n):
     sr = r
@@ -52,8 +58,7 @@ def diagonalCheckUR(r, c, n):
     if chain == 5:
         if sr == 18 or sc == 0 or board[sr+1][sc-1] != n:
             return True
-        else:
-            return False
+    return False
 
 winner = 0
 wr = ''
