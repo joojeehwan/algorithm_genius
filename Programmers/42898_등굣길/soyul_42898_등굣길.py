26a3113ee2e7807e29925c5d9113dd117946c4b7
zoyul <psa9456@gmail.com>
2022-06-08 00:21:26 +0900

    42898 등굣길

--- Programmers/42898_등굣길/soyul_42898_등굣길.py
+++ Programmers/42898_등굣길/soyul_42898_등굣길.py
@@ -1,3 +1,32 @@
+def solution(m, n, puddles):
+
+    dp = [[0] * m for _ in range(n)]
+    dp[0][0] = 1
+
+    for j in range(1, m):              # 맨 윗줄 먼저 채우기
+        if [j+1, 1] in puddles:        # 웅덩이면 0으로
+            dp[0][j] = 0
+            continue
+        dp[0][j] += dp[0][j-1]
+
+    for i in range(1, n):               # 맨 왼쪽줄 먼저 채우기
+        if [1, i+1] in puddles:         # 웅덩이면 0
+            dp[i][0] = 0
+            continue
+        dp[i][0] += dp[i-1][0]
+
+    for i in range(1, n):
+        for j in range(1, m):
+            if [j+1, i+1] in puddles:           # 웅덩이면 패스
+                continue
+            dp[i][j] = dp[i-1][j] + dp[i][j-1]
+
+    answer = dp[-1][-1]
+    return answer % (1000000007)
+
+"""
+bfs로 풀었더니 시간초과!
+
 from collections import deque
 
 def bfs(m, n, puddles):
@@ -38,3 +67,5 @@ def solution(m, n, puddles):
 
 
 print(solution(4, 3, [[2, 2]]))
+
+"""
