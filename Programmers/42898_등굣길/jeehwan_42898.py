8c5ad1dd4168a9f9acd0fb94a53855a3c299add0
joojeehwan <meanstrike94@gmail.com>
2022-06-10 15:54:52 +0900

    등굣실 풀이 다르게

--- Programmers/42898_등굣길/jeehwan_42898.py
+++ Programmers/42898_등굣길/jeehwan_42898.py
@@ -1,3 +1,5 @@
+#메오이제이션 dfs
+
 # def solution(m, n, puddles):
 #     #1 부터 시작하는 (1,1) ~ (n, m) 2차원 배열
 #     memo = [[0 for _ in range(m + 1)] for _ in range(n+1)] # 0으로 초기화
@@ -17,8 +19,35 @@
 #         return memo[row][col]
 #     return dp(n, m) % 1000000007
 
+# 단순 반복문 풀이
+
+# def solution(m, n, puddles):
+#     # 아 이렇게 좌표를 거꾸로 할 수 있구나
+#     puddles = [[q, p] for [p, q] in puddles]
+#     # dp 초기화 => 이차원 배열
+#
+#     dp = [[0] * (m + 1) for i in range(n + 1)]
+#     # dp = [[0 for _ in range(m + 1)] for _ in range(n + 1)]
+#     # print(dp)
+#     dp[1][1] = 1
+#
+#     for i in range(1, n + 1):
+#         for j in range(1, m + 1):
+#             if i == 1 and j == 1:
+#                 continue
+#
+#             # in 메서드를 사용해서 현재 i, j가 물 웅덩이 인지 확인
+#             if [i, j] in puddles:
+#                 dp[i][j] = 0
+#
+#             else:
+#                 dp[i][j] = (dp[i - 1][j] + dp[i][j - 1]) % 1000000007
+#
+#     return dp[n][m]
+
 # bfs 풀이
 
+#오른쪽, 아래로
 dr = [0, 1]
 dc = [1, 0]
 
@@ -31,6 +60,8 @@ def solution(m, n, puddles):
     MAP = [[0] * m for _ in range(n)]
     MAP[0][0] = 1
     # 2. 초기값 구성
+    # q = deque()
+    # q.append((0, 0))
     q = deque([(0, 0)])
     while q:
         row, col = q.popleft()
