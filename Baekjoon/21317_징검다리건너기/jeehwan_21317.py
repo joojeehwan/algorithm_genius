b461c530c08145866b0a30dac6cce503cff0a807
jeehwan <meanstrike94@gmail.com>
2022-08-13 23:38:48 +0900

    0813 algo

--- Baekjoon/21317_징검다리건너기/jeehwan_21317.py
+++ Baekjoon/21317_징검다리건너기/jeehwan_21317.py
@@ -14,7 +14,6 @@ dp 풀이
 
 '''
 
-
 import sys
 input = sys.stdin.readline
 n = int(input())
@@ -34,6 +33,53 @@ for i in range(3, n):
     dp[i][1] = min(dp[i][1], dp[i-3][0]+k)
 
 print(min(dp[n-1][0], dp[n-1][1]))
+
+import sys
+input = sys.stdin.readline
+
+
+
+
+N = int(input())
+stone = []
+
+
+#dp 배열 생성
+
+dp = [1e9] * N
+dp[0] = 0
+
+#매우 큰 점프 없이,작은점프, 큰점프 만을 이용해서 실행해보기!
+for i in range(N-1):
+
+    j1, j2 = map(int, input().split())
+    stone.append((j1, j2))
+
+    if i + 1 < N:
+        dp[i+1] = min(dp[i+1], dp[i] + j1)
+
+    if i + 2 < N:
+        dp[i+2] = min(dp[i+2], dp[i] + j2)
+
+
+#매우 큰 점프를 적용해보기!
+
+K = int(input())
+
+MIN = dp[-1]
+
+for i in range(3, N):
+    e, dp1, dp2 = dp[i-3] + K , 1e9, 1e9
+    for j in range(i, N-1):
+        if i + 1 <= N:
+            dp1 = min(dp1, e + stone[j][0])
+        if i + 2 <= N:
+            dp2 = min(dp2, e + stone[j][1])
+        e, dp1, dp2 = dp1, dp2, 1e9
+
+    MIN = min(MIN, e)
+
+print(MIN)
 
 
 
@@ -58,3 +104,56 @@ def dfs(isK, idx, sum):
 
 dfs(False, 0, 0)
 print(ans)
+
+# global ans
+# ans = int(1e9)
+#
+# N = int(input())
+# arr = [list(map(int, input().split())) for _ in range(N - 1)]
+# K = int(input())
+#
+#
+# def dfs(idx, cost, status):
+#     global ans
+#     if idx == N - 1:
+#         if ans > cost:
+#             ans = cost
+#         return
+#
+#     if idx + 1 < N:
+#         dfs(idx + 1, cost + arr[idx][0], status)
+#     if idx + 2 < N:
+#         dfs(idx + 2, cost + arr[idx][1], status)
+#     if idx + 3 < N and status:
+#         dfs(idx + 3, cost + K, False)
+#
+#
+# dfs(0, 0, True)
+# print(ans)
+global ans
+ans = int(1e9)
+
+
+N = int(input())
+arr = [list(map(int, input().split())) for _ in range(N - 1)]
+k = int(input())
+
+def dfs(lev, cost, status):
+
+    global ans
+
+    if lev == N - 1:
+        #최솟값 찾는 로직
+        if ans > cost:
+            ans = cost
+        return
+
+    dfs(lev + 1, cost + arr[lev][0], status)
+    dfs(lev + 2, cost + arr[lev][1], status)
+
+    if lev + 3 < N and status:
+        dfs(lev + 3, cost + K, False)
+
+dfs(0, 0, True)
+print(ans)
+
