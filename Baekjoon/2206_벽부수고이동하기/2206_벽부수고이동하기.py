03fe1049a4a5647f95da56007f63412083233e66
joojeehwan <meanstrike94@gmail.com>
2022-10-28 01:42:29 +0900

    algo 1028 algo

--- Baekjoon/2206_벽부수고이동하기/2206_벽부수고이동하기.py
+++ Baekjoon/2206_벽부수고이동하기/2206_벽부수고이동하기.py
@@ -74,40 +74,58 @@ def bfs(x, y, z):
 print(bfs(0, 0, 0))
 
 
+
+
 from collections import deque
 
-def bfs(i, j, wall):               # k는 벽 부수기 여부 (1:부숨)
-    deq = deque()
-    deq.append((i, j, wall))
-    visited[i][j][wall] = 1        # 방문 체크
 
-    while deq:
-        i, j, wall = deq.popleft()
+#벡터 배열 #상 하 좌 우
 
-        if i == n-1 and j == m-1:
-            return visited[i][j][wall]
+dr = [-1, 1, 0, 0]
+dc = [0, 0, -1, 1]
 
-        for k in range(4):
-            ni = i + di[k]
-            nj = j + dj[k]
-            if 0 <= ni < n and 0 <= nj < m and visited[ni][nj][wall] == 0:      # 방문 안 했으면
-                if mapp[ni][nj] == 0:                                           # 벽 없으면
-                    visited[ni][nj][wall] = visited[i][j][wall] + 1             # 방문 체크
-                    deq.append((ni, nj, wall))
-
-                if mapp[ni][nj] == 1 and wall == 0:                          # 벽이 있고 안 부쉈으면
-                    visited[ni][nj][1] = visited[i][j][wall] + 1             # 벽 부순 상태 방문 체크
-                    deq.append((ni, nj, 1))
-
-    return -1
+def bfs(row, col, wall = 0):
+    q = deque()
+    q.append((row, col, wall))
+    visited[row][col] = 1
 
 
+    while q:
+
+        now_row, now_col, now_wall = q.popleft()
+
+        # 원하는 도착 지점에 도착하면, bfs 종료
+        if now_row == n-1 and now_col == m - 1 :
+            return visited[row][col][wall]
+
+
+        for dir in range(4):
+
+            next_row = now_row + dr[dir]
+            next_col = now_col + dc[dir]
+
+            #범위 체크
+            if 0 <= next_row < n and 0 <= next_col <m:
+
+                #한번도 안 가본 곳 이면서, 벽이 없는 곳
+                if not visited[next_row][next_col][wall] and MAP[next_row][next_col] == 0:
+                    visited[next_row][next_col][wall] = visited[now_row][now_col] + 1
+                    q.append((next_row, next_col, wall))
+
+                # 한번도 안 가본 곳 이면서, 벽이 있는 곳
+                if not visited[next_row][next_col][wall] and MAP[next_row][next_col] == 1:
+                    visited[next_row][next_col][wall] = visited[now_row][now_col] + 1
+                    q.append((next_row, next_col, 1))
+
+
+
+
+#초기 입력받기
 n, m = map(int, input().split())
-mapp = [list(map(int, input())) for _ in range(n)]
+MAP = [list(map(int, input())) for _ in range(n)]
 
-di = [0, 1, 0, -1]  # 우하좌상 탐색
-dj = [1, 0, -1, 0]
+# 이 문제의 포인트..! 삼차원 배열로! 표시하기 // 배열의 값은 그떄까지의 최단 경로를 담기
 
 visited = [[[0] * 2 for _ in range(m)] for _ in range(n)]
 
-print(bfs(0, 0, 0))
+
