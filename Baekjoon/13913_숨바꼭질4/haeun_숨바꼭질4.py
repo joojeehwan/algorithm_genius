41fe7a7a1f907b2bc9b20a92ce28c9f500c2aae0
pear96 <bhaeun1105@gmail.com>
2022-12-14 02:31:20 +0900

    1214 - 백준 13913 숨바꼭질4 개선

--- Baekjoon/13913_숨바꼭질4/haeun_숨바꼭질4.py
+++ Baekjoon/13913_숨바꼭질4/haeun_숨바꼭질4.py
@@ -5,12 +5,24 @@ MAX_VALUE = 100000
 
 n, k = map(int, input().split())
 visited = [0] * (MAX_VALUE + 1)
-visited[n] = 1
+routes = [0] * (MAX_VALUE + 1)
 
+visited[n] = 1
 queue = deque([n])
 
 
-def find_short_time():
+def search():
+    path = [k]
+    prev = k
+
+    for _ in range(visited[k]-1):
+        path.append(routes[prev])
+        prev = routes[prev]
+
+    print(*path[::-1])
+
+
+def bfs():
     while not visited[k]:
         now_pos = queue.popleft()
 
@@ -20,26 +32,10 @@ def find_short_time():
             if MIN_VALUE <= new_pos <= MAX_VALUE and \
                     not visited[new_pos]:
                 visited[new_pos] = visited[now_pos] + 1
+                routes[new_pos] = now_pos
                 queue.append(new_pos)
-    return visited[k]-1
+    print(visited[k]-1)
+    search()
 
 
-def find_short_route():
-    next_pos = k
-    routes = [k]
-
-    while next_pos != n:
-        previous_time = visited[next_pos] - 1
-        if next_pos % 2 == MIN_VALUE and visited[next_pos // 2] == previous_time:
-            next_pos //= 2
-        elif next_pos - 1 >= MIN_VALUE and visited[next_pos - 1] == previous_time:
-            next_pos -= 1
-        elif next_pos + 1 <= MAX_VALUE and visited[next_pos + 1] == previous_time:
-            next_pos += 1
-        routes.append(next_pos)
-
-    return " ".join(map(str, reversed(routes)))
-
-
-print(find_short_time())
-print(find_short_route())
+bfs()
