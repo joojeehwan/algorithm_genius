c2a6e7893ee7147eab74bdf83c08caa51aa57f11
jeehwan <meanstrike94@gmail.com>
2023-02-11 09:43:19 +0900

    0211 - 계단 오르기 인덱스 에러 수정

--- Baekjoon/2579_계단오르기/jeehwan_2579.py
+++ Baekjoon/2579_계단오르기/jeehwan_2579.py
@@ -12,23 +12,45 @@
 
 
 
+마지막 도착점에 도착하기 위해서 잘 생각해보면,
 
+마지막 도착 계단 전에 계단을 밝고 오는 경우와
+
+마지막 도착 계단 전에 계닫을 밝지 않고, 2칸전에서 오는 경우가 있을 수 있음
+
+그런데 3개의 계단을 연속으로 갈 수 없는 조건도 생각해야 하기 떄문에
+
+
+ex) dp[3]의 경우
+
+dp[3] = max(dp[1] + stair[3], dp[0] + stair[2] + stair[3])
 '''
 
 N = int(input())
 
-stair = [0] * 301
+stair = [0] * (N + 1)
 
-for i in range(N):
+for i in range(1, N + 1):
     stair[i] = int(input())
 
+DP = [0] * (N + 1)
 
 
-DP = [0] * 301
-DP[0] = stair[0]
-DP[1] = stair[0] + stair[1]
-DP[2] = max(stair[0] + stair[2], stair[1] + stair[2])
+# 예외 처리를 해주지 않으면, 인덱스 에러난다...!
+if N == 1:
+    print(stair[N])
 
+elif N == 2:
+    print(stair[1] + stair[2])
 
-for i in range(3, N):
-    DP[i] = max(DP[i-3] + stair[i-1] + stair[i], DP[i-2] + stair[i])
+else :
+    DP[1] = stair[1]
+    DP[2] = stair[1] + stair[2]
+    DP[3] = max(stair[2] + stair[3], stair[1] + stair[3])
+
+    for i in range(4, N + 1):
+        # 3개 연속 밝으면 안된다.
+
+        DP[i] = max(DP[i-3] + stair[i-1] + stair[i], DP[i-2] + stair[i])
+
+    print(DP[N])
