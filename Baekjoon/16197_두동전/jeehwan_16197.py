3c3db797d91fefd5f75ed19004963acafe2ae8b8
joojeehwan <meanstrike94@gmail.com>
2023-03-08 17:29:24 +0900

    23.03.08 두동전

--- Baekjoon/16197_두동전/jeehwan_16197.py
+++ Baekjoon/16197_두동전/jeehwan_16197.py
@@ -88,9 +88,219 @@ def dfs(coin1, coin2, cnt):
 
         # 이동 후 범위체크
 
+        #두 동전의 이동이 모두 MAP 안에서 이루어질 때
         if 0 <= next_row1 < N and 0 <= next_col1 < M and 0 <= next_row2 < N and 0 <= next_col2 < M:
-            pass
+            # 두 동전 다음이 벽이 아닌 곳으로 이동
+            if MAP[next_row1][next_col1] != "#" and MAP[next_row2][next_col2] != "#" and (next_row1, next_col1, next_row2, next_col2) not in visited :
+                visited.append((next_row1, next_col1, next_row2, next_col2))
+                dfs((next_row1, next_col1), (next_row2, next_col2), cnt + 1)
+                visited.pop()
+            # 1번 동전은 벽이 있는 곳으로, 2번 동전은 정상적으로 이동
+            elif MAP[next_row1][next_col1] == "#" and MAP[next_row2][next_col2] != "#" and (row1, row2, next_row2 , next_col2) not in visited :
+                visited.append((next_row1, next_col1, next_row2, next_col2))
+                dfs((next_row1, next_col1), (next_row2, next_col2), cnt + 1)
+                visited.pop()
+            # 2번 동전은 벽이 있는 곳으로, 1번 동전은 정상적으로 이동
+            elif MAP[next_row1][next_col1] != "#" and MAP[next_row2][next_col2] == "#" and (next_row1, next_col1, row2, col2) not in visited :
+                visited.append((next_row1, next_col1, next_row2, next_col2))
+                dfs((next_row1, next_col1), (next_row2, next_col2), cnt + 1)
+                visited.pop()
 
 
+        # 하나 이상의 동전이 이동후 MAP 밖으로
         else:
-            pass
+            visited.append((next_row1, next_col1, next_row2, next_col2))
+            dfs((next_row1, next_col1), (next_row2, next_col2), cnt + 1)
+            visited.pop()
+
+
+for row in range(N):
+    for col in range(M):
+        if MAP[row][col] == "0" :
+            coins_position.append((row, col))
+
+
+dfs(coins_position[0], coins_position[1], 0)
+print(ans if ans <= 10 else print(-1))
+
+
+
+#dfs 풀이 2
+
+import sys
+
+def solve(r1, c1, r2, c2, depth):
+    global answer
+    if depth >= 10:
+        return
+
+    for i in range(4):
+        nr1, nc1 = r1+dr[i], c1+dc[i]
+        nr2, nc2 = r2+dr[i], c2+dc[i]
+        move = [1, 1]
+        # 둘 다 나갔다 continue, 이 밑으로는 적어도 하나는 안에 있다
+        if not verify(nr1, nc1) and not verify(nr2, nc2):
+            continue
+
+        # nr1, nc1이 안에 있으면서 다음께 # 이면 이동 안함
+        if verify(nr1, nc1) and adj[nr1][nc1] == '#':
+            nr1, nc1 = r1, c1
+            move[0] = 0
+        if verify(nr2, nc2) and adj[nr2][nc2] == '#':
+            nr2, nc2 = r2, c2
+            move[1] = 0
+
+        # 둘 다 이동 안한 경우도 지나쳐주자
+        if move[0] or move[1]:
+            continue
+
+        # 둘 다 안 나간 경우, 이동하자
+        if verify(nr1, nc1) and verify(nr2, nc2):
+            solve(nr1, nc1, nr2, nc2, depth+1)
+            continue
+
+        answer = min(depth+1, answer)
+
+
+# x1, y1, x2, y2 가 따로움직여야함.
+def verify(r, c):
+    if r < 0 or r >= N or c < 0 or c >= M:
+        return False
+    return True
+
+
+dr = [0, 0, 1, -1]
+dc = [1, -1, 0, 0]
+N, M = map(int, sys.stdin.readline().rstrip().split())
+adj = []
+coins = []
+answer = 0xFF
+for r in range(N):
+    road = list(sys.stdin.readline().rstrip())
+    adj.append(road)
+    for i, c in enumerate(road):
+        if c == 'o':
+            coins.append([r, i])
+
+solve(coins[0][0], coins[0][1], coins[1][0], coins[1][1], 0)
+if answer == 0xFF:
+    print(-1)
+else:
+    print(answer)
+
+
+#bfs풀이 ( 이 풀이가 제일 깔끔)
+
+from collections import deque
+import sys
+
+input = sys.stdin.readline
+
+dx = [-1, 0, 1, 0]
+dy = [0, -1, 0, 1]
+
+def bfs():
+    while coin:
+        x1, y1, x2, y2, cnt = coin.popleft()
+
+        if cnt >= 10:
+            return -1
+
+        for i in range(4):
+            nx1 = x1 + dx[i]
+            ny1 = y1 + dy[i]
+            nx2 = x2 + dx[i]
+            ny2 = y2 + dy[i]
+
+            if 0 <= nx1 < n and 0 <= ny1 < m and 0 <= nx2 < n and 0 <= ny2 < m:
+                # 벽이라면
+                if board[nx1][ny1] == "#":
+                    nx1, ny1 = x1, y1
+                if board[nx2][ny2] == "#":
+                    nx2, ny2 = x2, y2
+                coin.append((nx1, ny1, nx2, ny2, cnt + 1))
+            elif 0 <= nx1 < n and 0 <= ny1 < m:  # coin2가 떨어진 경우
+                return cnt + 1
+            elif 0 <= nx2 < n and 0 <= ny2 < m:  # coin1가 떨어진 경우
+                return cnt + 1
+            else:  # 둘 다 빠진 경우 무시
+                continue
+
+    return -1
+
+
+if __name__ == "__main__":
+    n, m = map(int, input().split())
+
+    coin = deque()
+    board = []
+    temp = []
+    for i in range(n):
+        board.append(list(input().strip()))
+        for j in range(m):
+            if board[i][j] == "o":
+                temp.append((i, j))
+
+    coin.append((temp[0][0], temp[0][1], temp[1][0], temp[1][1], 0))
+
+    print(bfs())
+
+#bfs풀이2
+
+from collections import deque
+import sys
+
+def solve():
+    queue = deque([(coin[0], coin[1], coin[2], coin[3], 0)])
+    dr = [0, 0, 1, -1]
+    dc = [1, -1, 0, 0]
+    while queue:
+        r1, c1, r2, c2, cnt = queue.popleft()
+        if cnt >= 10:
+            break
+
+        for i in range(4):
+            nr1, nc1 = r1+dr[i], c1+dc[i]
+            nr2, nc2 = r2+dr[i], c2+dc[i]
+            move = [1, 1]
+            # 둘 다 나간 경우
+            if not (0 <= nr1 < N and 0 <= nc1 < M) or not (0 <= nr2 < N and 0 <= nc2 < M):
+            # if not verify(nr1, nc1) and not verify(nr2, nc2):
+                continue
+
+            # 하나만 나간 경우
+            if not (0 <= nr1 < N and 0 <= nc1 < M) or not (0 <= nr2 < N and 0 <= nc2 < M):
+            # if not verify(nr1, nc1) or not verify(nr2, nc2):
+                return cnt + 1
+
+            # 둘 다 안나간 경우
+            if adj[nr1][nc1] == '#':
+                nr1, nc1 = r1, c1
+                move[0] = 0
+            if adj[nr2][nc2] == '#':
+                nr2, nc2 = r2, c2
+                move[1] = 0
+            if move[0] or move[1]:
+                queue.append((nr1, nc1, nr2, nc2, cnt+1))
+    return -1
+
+
+def verify(r, c):
+    if 0 <= r < N and 0 <= c < M:
+        return True
+    return False
+
+N, M = map(int, sys.stdin.readline().rstrip().split())
+adj = []
+coin = []
+
+for r in range(N):
+    road = list(sys.stdin.readline().rstrip())
+    adj.append(road)
+    for c in range(M):
+        if road[c] == 'o':
+            coin.extend([r, c])
+
+answer = solve()
+print(answer)
+
