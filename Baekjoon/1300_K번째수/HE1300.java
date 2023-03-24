c9638450b01f3161ca6a7aad5011196bdb83be74
pear96 <bhaeun1105@gmail.com>
2023-03-25 02:00:18 +0900

    0324 백준 1300 K번째수 주석추가
    - 이분탐색은 정말 어려운 거였다.

--- Baekjoon/1300_K번째수/HE1300.java
+++ Baekjoon/1300_K번째수/HE1300.java
@@ -1,7 +1,8 @@
 /*
  * 공부하기 좋은 링크 : https://st-lab.tistory.com/281
  * - parameter값 설정까진 성공했으나, 구하는 계산식을 떠올리지 못 했다.
- * 약수의 개수를 찾을까 했는데, 그냥 나누면 되는 것이다.
+ * 	 약수의 개수를 찾을까 했는데, 그냥 나누면 되는 것이다.
+ * - low가 정답일까, high가 정답일까? 
  * */
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -19,6 +20,7 @@ public class HE1300 {
 		
 		// low와 high가 경계를 두고 서있기 위해 1차이가 날때 끝난다.
 		// 범위를 반씩 줄여나가는게 이분탐색이므로, 시간 복잡도는 O(logN)
+		// high를 답으로 설정하는 이유는, mid가 K랑 같을 때 뒤는 잘라도 되지만 앞은 자르면 안되기 때문이다.
 		while(low+1 < high) {
 			long mid = (low + high) / 2;
 			long idx = 0; // mid라는 수는 같거나 작은 수가 몇개인지 세기 위함
@@ -38,3 +40,10 @@ public class HE1300 {
 	}
 
 }
+
+/* chatGPT의 시간 복잡도 답변
+ * 이 코드의 시간 복잡도는 O(NlogN)입니다. 
+ * 이분 탐색을 수행하는 동안, for 루프가 N번 실행되며 각각의 루프에서 Math.min() 함수도 호출됩니다. 
+ * 이 루프에서 최대 k번까지 실행될 수 있습니다. 
+ * 따라서 while 루프 내에서 실행되는 모든 작업의 총 시간 복잡도는 O(NklogN)이며, k는 N보다 작거나 같으므로 O(NlogN)이 됩니다.
+ * */
