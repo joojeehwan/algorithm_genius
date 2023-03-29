50eca55bd694a33fa993f64cc6adb804cf678ca1
pear96 <bhaeun1105@gmail.com>
2023-03-30 00:14:34 +0900

    0330 코드트리 회전하는 빙하 업데이트
    - 90도 회전을 4방향 이동으로 구현했다.
    - 와중에 우 하 좌 상으로 했더니 실수했다.
    - 실수 연발 그만~!~!

--- CodeTree/회전하는빙하/haeun_회전하는빙하.py
+++ CodeTree/회전하는빙하/haeun_회전하는빙하.py
@@ -10,11 +10,11 @@ N, Q = map(int, input().split())
 ice = list(list(map(int, input().split())) for _ in range(2**N))
 levels = list(map(int, input().split()))  # python이라 딱히 Q 필요 없음
 
-# 필요 변수
-dr = [-1, 1, 0, 0]
-dc = [0, 0, -1, 1]
+# 필요 변수 (빙하 회전을 위해 우하좌상으로 만든다)
+dr = [0, 1, 0, -1]
+dc = [1, 0, -1, 0]
 length = 2 ** N
-rotated = [[0] * length for _ in range(length)]
+rotated = [[0] * length for _ in range(length)]    # 원래 격자 만큼의 임시 배열을 만든다. 회전하면서 여기다 옮겨줌
 
 
 def print_ice(message):
@@ -24,22 +24,15 @@ def print_ice(message):
     print()
 
 
-# 4등분된 빙하를 한 덩이씩 보면서 회전하는 함수
-def rotating(sr, sc, l):
-
-    l_len = 2 ** l  # level의 길이
-    s_len = 2 ** (l-1)  # level-1의 길이
-    temp = [[0] * l_len for _ in range(l_len)]
-
-    for r in range(0, l_len, s_len):
-        for c in range(0, l_len, s_len):
-            for _r in range(s_len):
-                for _c in range(s_len):
-                    temp[c+_r][s_len-r+_c] = ice[sr+r+_r][sc+c+_c]
-
-    for r in range(l_len):
-        for c in range(l_len):
-            rotated[sr+r][sc+c] = temp[r][c]
+# 2^l 크기의 격자를 선택해서 들어왔다.
+# 2^(l-1) 크기로 4등분된 빙하를 한 덩이씩 보면서 회전하는 함수
+# d(방향) 에 맞춰 옮겨준다.
+def rotating(sr, sc, half, d):
+    for r in range(sr, sr+half):
+        for c in range(sc, sc+half):
+            nr = r + dr[d] * half
+            nc = c + dc[d] * half
+            rotated[nr][nc] = ice[r][c]
 
 
 # 빙하를 선택하는 함수
@@ -48,17 +41,23 @@ def rotating(sr, sc, l):
 def iceberg_rotate(level):
     if level == 0:
         return
-    global rotated
-    # 원래 격자 만큼의 임시 배열을 만든다. 회전하면서 여기다 옮겨줌
-    rotated = [[0] * length for _ in range(length)]
+    l_len = 2 ** level
+    h_len = l_len // 2
 
-    for start_r in range(0, length, 2 ** level):
-        for start_c in range(0, length, 2 ** level):
-            rotating(start_r, start_c, level)
+    # 4등분으로 반시계 방향으로 90' 회전하지만, 꼭 회전공식을 사용하지 않아도 된다.
+    # 좌상, 우상, 우하, 좌하 격자 순대로 우 -> 하 -> 좌 -> 상의 방향으로 격자를 옮기는 것과 같은 행위다.
+    # 이 생각을 떠올리긴 했지만, 4경우 모두 하나의 for 반복문 내에서 처리하려고 한 점이 더 돌아가게 만들었다.
+    for start_r in range(0, length, l_len):
+        for start_c in range(0, length, l_len):
+            rotating(start_r, start_c, h_len, 0)
+            rotating(start_r, start_c + h_len, h_len, 1)
+            rotating(start_r+h_len, start_c+h_len, h_len, 2)
+            rotating(start_r+h_len, start_c, h_len, 3)
 
     # 회전 사항 반영
     for r in range(length):
         ice[r] = rotated[r][:]
+        rotated[r] = [0] * length
 
 
 # 빙하가 녹는 함수
