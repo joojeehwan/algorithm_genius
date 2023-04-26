fe268be0c186b281986027bc80b2eb973ee09c35
jeehwan <meanstrike94@gmail.com>
2023-04-27 04:35:30 +0900

    0427 바이러스

--- Programmers/154540_무인도여행/jeehwan_154540.py
+++ Programmers/154540_무인도여행/jeehwan_154540.py
@@ -66,30 +66,6 @@ print(solution(["XXX","XXX","XXX"]))
 
 # dfs 풀이
 
-
-7
-8
-9
-10
-11
-12
-13
-14
-15
-16
-17
-18
-19
-20
-21
-22
-23
-24
-25
-26
-27
-28
-29
 def solution(maps):
     import sys
     sys.setrecursionlimit(int(1e9))
